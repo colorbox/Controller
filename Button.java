aeb0626047f0734c95b112a95857b6837ae41376
colorbox <colorbox222@gmail.com>
2012-10-28 21:23:18 +0900

    complete ...maybe

--- Button.java
+++ Button.java
@@ -1,17 +1,17 @@
-package Controler;
+package Controller;
 
 import java.awt.event.*;
 
 public class Button{
-    private int Time;
     private int ButtonCode;
+    private int Time;
     private int LastPressTime;
     private int PressInterval;
-    private boolean Pressing;
+    private boolean Pressing=false;
 
     public void setButtonCode(int ButtonCode){this.ButtonCode=ButtonCode;}
     public void setTime(int Time){this.Time=Time;}
-    public void setLastPressTime(int LastPressTIme){this.LastPressTime=LastPressTime;}
+    public void setLastPressTime(int LastPressTime){this.LastPressTime=LastPressTime;}
     public void setPressInterval(int PressInterval){this.PressInterval=PressInterval;}
     public void setPressing(boolean Pressing){this.Pressing = Pressing;}
 
@@ -21,28 +21,33 @@ public class Button{
     public int getPressInterval(){return PressInterval;}
     public boolean getPressing(){return Pressing;}
 
-    public Button(int KeyCode){
+    public Button(int KeyCode,int PressInterval){
 	setButtonCode(KeyCode);
 	setTime(0);
-	setLastPressTime(0);
-	setPressInterval(8);
+	setPressInterval(PressInterval);
+	setLastPressTime( -1*getPressInterval() );
+	setPressing(false);
     }
 
     public boolean isPress(){return getPressing();}
 
+    public void resetButton(){setButton(false);}
+
     public void incrementTime(){
 	setTime(getTime()+1);
+	resetButton();
     }
 
     public void keyPressed(int KeyCode){
-	//if key code and argument match, time interval is enough,Button is not Pressing , then Press the Button.and renew LastPressTime.
-	if( getButtonCode() == KeyCode && getTime() - getLastPressTime() > getPressInterval() && !isPress()){
+	//if key code and argument match, time interval is enough,and Button is not Pressing , then Press the Button.
+	//And renew LastPressTime.
+	if( getButtonCode() == KeyCode && getTime() - getLastPressTime() >= getPressInterval() && !isPress()){
 	    Pressing = true;
 	    setLastPressTime(getTime());
 	}
     }
 
-    public void keyReleaseed(int KeyCode){
+    public void keyReleased(int KeyCode){
 	if( getButtonCode() == KeyCode ){
 	    Pressing = false;
 	}
--- Controller.java
+++ Controller.java
@@ -3,145 +3,45 @@ package Controller;
 import java.awt.event.*;
 
 public class Controller{
-    //timer
-    private int Time;
-    //スティック
-    private int stick=5;
-    //Zボタン、Xボタン,STARTボタン
-    private boolean ZPress=false,XPress=false,StatPress=false;
-    private int ZPressTime=0,XPressTime=0,StatPressTime=0;
-    //スティック
-    private boolean UpPress=false,DownPress=false,LeftPress=false,RightPress=false;
+    private ZButton,XButton;
+    private Stick stick;
 
-    public Controller(int Time){
-	setTime(Time);
+    public Controller(){
+	ZButton = new Button(KeyEvent.VK_Z,10);
+	XButton = new Button(KeyEvent.VK_X,10);
+	stick = new Stick();
     }
 
-    //スティックの状態取得
-    public int getStick(){return stick;}
-    //getter
-    public boolean getUpPress(){return UpPress;}
-    public boolean getDownPress(){return DownPress;}
-    public boolean getLeftPress(){return LeftPress;}
-    public boolean getRightPress(){return RightPress;}
-    public boolean getZPress(){return ZPress;}
-    public boolean getXPress(){return XPress;}
-    public boolean getStatPress(){return StatPress;}
-    public int getZPressTime(){return ZPressTime;}
-    public int getXPressTime(){return XPressTime;}
-    public int getStatPressTime(){return StatPressTime;}
-    public int getTime(){return Time;}
-    //setter
-    public void setUpPress(boolean UpPress){this.UpPress=UpPress;}
-    public void setDownPress(boolean DownPress){this.DownPress=DownPress;}
-    public void setLeftPress(boolean LeftPress){this.LeftPress=LeftPress;}
-    public void setRightPress(boolean RightPress){this.RightPress=RightPress;}
-    public void setZPress(boolean ZPress){this.ZPress=ZPress;}
-    public void setXPress(boolean XPress){this.XPress=XPress;}
-    public void setStatPress(boolean StatPress){this.StatPress=StatPress;}
-    public void setZPressTime(int ZPressTime){this.ZPressTime=ZPressTime;}
-    public void setXPressTime(int XPressTime){this.XPressTime=XPressTime;}
-    public void setStatPressTime(int StatPRessTime){this.StatPressTime=StatPressTime;}
-    public void setTime(int Time){this.Time=Time;}
-    //ボタンのリセット
-    public void buttonReset(){
-	setZPress(false);
-	setXPress(false);
-	setStatPress(false);
+    public boolean isZPress(){
+	return ZButton.isPress();
+    }
+    public boolean isXPress(){
+	return XPress.isPress();
     }
 
-    //proceeding Time
-    public void timeProceeding(){
-	setTime(getTime()+1);
+    public int getStickDirection(){
+	stick.getDirection();
     }
 
-    //ボタン状態判定
-    public void setButton(){
-	//各ボタンの状態で分岐
-	//最後にボタンが押されていた時間を取得
-	//現在の時間-最後に押された時間 が十分に大きく、かつボタンが押されていなければ、ボタンを押下状態にする	
-	if(getTime() - ZPressTime>10 && !ZPress){
-	    ZPress=true;
-	    ZPressTime=getTime();
-	}
-	if(getTime() - XPressTime>10 && !XPress){
-	    XPress=true;
-	    XPressTime=getTime();
-	}
+    public void incrementTime(){
+	ZButton.incrementTime();
+	XButton.incrementTime();
+	stick.incrementTime();
     }
 
-    //スティック状態判定
-    public void setStick(){
-	if(RightPress){
-	    stick=6;
-	}else if(LeftPress){
-	    stick=4;
-	}else if(UpPress){
-	    stick=8;
-	}else if(DownPress){
-	    stick=2;
-	}else{
-	    stick=5;
-	}
-	if(RightPress && UpPress){
-	    stick=9;
-	}else if(UpPress && LeftPress){
-	    stick=7;
-	}else if(LeftPress && DownPress){
-	    stick=1;
-	}else if(DownPress && RightPress){
-	    stick=3;
-	}
-    }
 
-    //キー押下時
     public void keyPressed(KeyEvent e){
 	int keycode = e.getKeyCode();
-	if(keycode == KeyEvent.VK_RIGHT){
-	    RightPress=true;
-	}
-	if(keycode == KeyEvent.VK_UP){
-	    UpPress=true;
-	}
-	if(keycode == KeyEvent.VK_LEFT){
-	    LeftPress=true;
-	}
-	if(keycode == KeyEvent.VK_DOWN){
-	    DownPress=true;
-	}
-	if(keycode == KeyEvent.VK_Z && getTime() - ZPressTime>10 && !ZPress){
-	    ZPress=true;
-	    setZPressTime(getTime());
-	}
-	if(keycode == KeyEvent.VK_X && getTime() - XPressTime>10 && !XPress){
-	    XPress=true;
-	    setXPressTime(getTime());
-	}
-	setStick();	
+	ZButton.keyPressed(keycode);
+	XButton.keyPressed(keycode);
+	stick.keyPressed(keycode);
     }
 
-    //キー離してる時
     public void keyReleased(KeyEvent e){
 	int keycode = e.getKeyCode();
-	if(keycode == KeyEvent.VK_RIGHT){
-	    RightPress=false;
-	}
-	if(keycode == KeyEvent.VK_UP){
-	    UpPress=false;
-	}
-	if(keycode == KeyEvent.VK_LEFT){
-	    LeftPress=false;
-	}
-	if(keycode == KeyEvent.VK_DOWN){
-	    DownPress=false;
-	}
-	if(keycode == KeyEvent.VK_Z){
-	    ZPress=false;
-	}
-	if(keycode == KeyEvent.VK_X){
-	    XPress=false;
-	}
-	setStick();
+	ZButton.keyReleased(keycode);
+	XButton.keyReleased(keycode);
+	stick.keyReleased(keycode);
     }
 }
 
